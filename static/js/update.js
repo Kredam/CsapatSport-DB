495ff35bf125d33812be9c28bf33f1df428e7582
Kredam <akitlosz2000@gmail.com>
2021-11-16 01:35:57 +0100

    gives input value based on which row button were clicked

--- static/js/update.js
+++ static/js/update.js
@@ -1,8 +1,16 @@
-var team_names = document.querySelectorAll("[id='specific_team_update']");
-for (const teamName of team_names) {
+var update_team = document.querySelectorAll("[id='specific_team_update']");
+for (const teamName of update_team) {
     teamName.addEventListener('click', ()=>{
-        selected_team_to_update = teamName.getAttribute('data-teamname')
-        document.getElementById('teams_name').value = selected_team_to_update;
+        selected_team_to_update = teamName.getAttribute('data-team')
+        document.getElementById('teams_name').value = selected_team_to_update.split('_').join(' ');
+    })
+}
+
+var delete_team = document.querySelectorAll("[id='remove_record']");
+for (const deleteTeamElement of delete_team) {
+    deleteTeamElement.addEventListener('click', ()=>{
+        selected_team_to_delete = deleteTeamElement.getAttribute('data-team')
+        document.getElementById('team_to_remove').value = selected_team_to_delete.split('_').join(' ')
     })
 }
 
