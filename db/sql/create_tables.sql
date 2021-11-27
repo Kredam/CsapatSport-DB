5b1167b05fc0df9612ff7791fd3bb4bea9a90aa1
kadam <Sheldorzo46>
2021-11-27 14:45:54 +0100

    created training grounds table instead of scorers

--- db/sql/create_tables.sql
+++ db/sql/create_tables.sql
@@ -12,15 +12,11 @@ CREATE TABLE Clubs(     name VARCHAR(50) NOT NULL,
                         D INT,
                         L INT);
 
-CREATE TABLE Scorers(   team VARCHAR(50) NOT NULL,
-                        league VARCHAR(50),
-                        name VARCHAR(100),
-                        goals INT);
-
 CREATE TABLE Players(   team VARCHAR(50) NOT NULL,
                         name VARCHAR(50),
                         position VARCHAR(30),
-                        number INT);
+                        number INT,
+                        goals INT);
 
 CREATE TABLE Matches(   team VARCHAR(50) NOT NULL,
                         enemy_team VARCHAR(50) NOT NULL,
@@ -29,6 +25,10 @@ CREATE TABLE Matches(   team VARCHAR(50) NOT NULL,
 CREATE TABLE Stadiums(  team VARCHAR(50) NOT NULL,
                         name VARCHAR(100),
                         city VARCHAR(100));
+
+CREATE TABLE TrainingGrounds(   team VARCHAR(50) NOT NULL,
+                                name VARCHAR(100),
+                                city VARCHAR(100));
 
 CREATE TABLE Managers(  name VARCHAR(50),
                         nationality VARCHAR(50),
@@ -40,9 +40,9 @@ CREATE TABLE Owners (   company VARCHAR(100),
 
 ALTER TABLE Clubs ADD PRIMARY KEY(name);
 ALTER TABLE Stadiums ADD PRIMARY KEY(name);
+ALTER TABLE TrainingGrounds ADD PRIMARY KEY(name);
 ALTER TABLE Championship ADD PRIMARY KEY(league, season);
 ALTER TABLE Matches ADD id INT PRIMARY KEY AUTO_INCREMENT;
-ALTER TABLE Scorers ADD id INT PRIMARY KEY AUTO_INCREMENT;
 ALTER TABLE Managers ADD id INT PRIMARY KEY AUTO_INCREMENT;
 ALTER TABLE Owners ADD id INT PRIMARY KEY AUTO_INCREMENT;
 ALTER TABLE Players ADD id INT PRIMARY KEY AUTO_INCREMENT;
@@ -51,7 +51,6 @@ ALTER TABLE Players ADD FOREIGN KEY (team) REFERENCES Clubs(name) ON UPDATE CASC
 ALTER TABLE Owners ADD FOREIGN KEY (team) REFERENCES Clubs(name) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE Matches ADD FOREIGN KEY (team) REFERENCES Clubs(name) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE Stadiums ADD FOREIGN KEY (team) REFERENCES Clubs(name) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE Scorers ADD FOREIGN KEY (league) REFERENCES Championship(league) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE Scorers ADD FOREIGN KEY (team) REFERENCES Clubs(name) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE TrainingGrounds ADD FOREIGN KEY (team) REFERENCES Clubs(name) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE Clubs ADD FOREIGN KEY(league, season) REFERENCES Championship(league, season) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE Managers ADD FOREIGN KEY(team) REFERENCES Clubs(name) ON UPDATE CASCADE ON DELETE CASCADE;
