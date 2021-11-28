432eb9ed8fa5f53d39e6d5508833338288256dc8
kadam <Sheldorzo46>
2021-11-28 15:17:33 +0100

    exception handling done

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import mysql.connector.errors
 from flask import Flask, redirect, url_for, render_template, request
 import os
 from db import database
@@ -13,6 +14,7 @@ from db.DAO import TrainingGroudnsDAO
 app = Flask(__name__, template_folder=os.path.abspath('client/templates'))
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./images/upload"
+app.secret_key = "abc"
 clubsDAO = ClubsDAO.ClubsDAO(database)
 playersDAO = PlayersDAO.PlayerDAO(database)
 matchesDAO = MatchesDAO.MatchesDAO(database)
@@ -29,161 +31,211 @@ def home():
 
 @app.route('/teams', methods=['GET', 'POST'])
 def teams():
+    error = None
+    championships = clubsDAO.getAllLeagues()
     if request.method == 'POST':
         clubsDAO.listClubs()
         if request.form.get('list') is not None and request.form.get('search_word').strip() != "":
             clubsDAO.searchClub(request.form.get('search_word'))
         elif request.form.get('update') is not None:
-            clubsDAO.updateClub(request.form.get('teams_name'), request.form.get('abbreviation'),
-                                request.form.get('badge'), request.form.get('league_name'),
-                                request.form.get('league_season'), request.form.get('wins'),
-                                request.form.get('draws'), request.form.get('loss'))
+            try:
+                clubsDAO.updateClub(request.form.get('teams_name'), request.form.get('abbreviation'),
+                                    request.form.get('badge'), request.form.get('league_name'),
+                                    request.form.get('league_season'), request.form.get('wins'),
+                                    request.form.get('draws'), request.form.get('loss'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
         elif request.form.get('create') is not None:
-            clubsDAO.addClub(request.form.get('teams_name'), request.form.get('abbreviation'),
-                             request.form.get('badge'), request.form.get('league_name'),
-                             request.form.get('league_season'), request.form.get('wins'),
-                             request.form.get('draws'), request.form.get('loss'))
+            try:
+                clubsDAO.addClub(request.form.get('teams_name'), request.form.get('abbreviation'),
+                                 request.form.get('badge'), request.form.get('league_name'),
+                                 request.form.get('league_season'), request.form.get('wins'),
+                                 request.form.get('draws'), request.form.get('loss'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
         elif request.form.get('remove_record') is not None:
             clubsDAO.removeClub(request.form.get('record_key'))
     else:
         clubsDAO.listClubs()
-    return render_template("csapatok.html", teams=database.cursor)
+    return render_template("csapatok.html", teams=database.cursor, championships=championships, error=error)
 
 
 @app.route('/players', methods=['GET', 'POST'])
 def players():
+    error = None
     if request.method == "POST":
         playersDAO.listPlayers()
         if request.form.get('list') is not None and request.form.get('search_word').strip() != "":
             playersDAO.searchPlayer(request.form.get('search_word'))
         elif request.form.get('update') is not None:
-            playersDAO.updatePlayer(id=request.form.get('player_id'),
-                                    team=request.form.get('teams_name'),
-                                    position=request.form.get('pitch_position'),
-                                    shirt_number=request.form.get('shirt_number'))
+            try:
+                playersDAO.updatePlayer(id=request.form.get('player_id'),
+                                        team=request.form.get('teams_name'),
+                                        position=request.form.get('pitch_position'),
+                                        shirt_number=request.form.get('shirt_number'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
         elif request.form.get('remove_record') is not None:
             playersDAO.removePlayer(request.form.get('record_key'))
         elif request.form.get('create') is not None:
-            playersDAO.createPlayer(team=request.form.get('teams_name'),
-                                    name=request.form.get('player_name'),
-                                    position=request.form.get('pitch_position'),
-                                    shirt_number=request.form.get('shirt_number'))
+            try:
+                playersDAO.createPlayer(team=request.form.get('teams_name'),
+                                        name=request.form.get('player_name'),
+                                        position=request.form.get('pitch_position'),
+                                        shirt_number=request.form.get('shirt_number'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
     else:
         playersDAO.listPlayers()
-    return render_template("jatekosok.html", players=database.cursor)
+    return render_template("jatekosok.html", players=database.cursor, error=error)
 
 
 @app.route('/matches', methods=['GET', 'POST'])
 def matches():
+    error=None
     if request.method == "POST":
         matchesDAO.listMatches()
         if request.form.get('list') is not None and request.form.get('search_word').strip() != "":
             matchesDAO.searchMatch(request.form.get('search_word'))
         elif request.form.get('update') is not None:
-            matchesDAO.updateMatch(request.form.get('match_id'), request.form.get('teams_name'),
-                                   request.form.get('enemy_team'), request.form.get('takes_place'))
+            try:
+                matchesDAO.updateMatch(request.form.get('match_id'), request.form.get('teams_name'),
+                                       request.form.get('enemy_team'), request.form.get('takes_place'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
         elif request.form.get('remove_record') is not None:
             matchesDAO.removeMatch(request.form.get('record_key'))
         elif request.form.get('create') is not None:
-            print(request.form)
-            matchesDAO.createMatch(request.form.get('teams_name'),
-                                   request.form.get('enemy_team'),
-                                   request.form.get('takes_place'))
+            try:
+                matchesDAO.createMatch(request.form.get('teams_name'),
+                                       request.form.get('enemy_team'),
+                                       request.form.get('takes_place'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
     else:
         matchesDAO.listMatches()
-    return render_template("meccsek.html", matches=database.cursor)
+    return render_template("meccsek.html", matches=database.cursor, error=error)
 
 
 @app.route('/stadiums', methods=['GET', 'POST'])
 def stadiums():
+    error = None
     if request.method == "POST":
         stadiumDAO.listStadium()
         if request.form.get('list') is not None and request.form.get('search_word').strip() != "":
             stadiumDAO.searchStadium(request.form.get('search_word'))
         elif request.form.get('update') is not None:
-            stadiumDAO.updateStadium(request.form.get('teams'),
-                                     request.form.get('stadium'),
-                                     request.form.get('city'))
+            try:
+                stadiumDAO.updateStadium(request.form.get('teams'),
+                                         request.form.get('stadium'),
+                                         request.form.get('city'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
         elif request.form.get('remove_record') is not None:
             stadiumDAO.removeStadium(request.form.get('record_key'))
         elif request.form.get('create') is not None:
-            stadiumDAO.createStadium(request.form.get('teams'),
-                                     request.form.get('stadium'),
-                                     request.form.get('city'))
+            try:
+                stadiumDAO.createStadium(request.form.get('teams'),
+                                         request.form.get('stadium'),
+                                         request.form.get('city'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
     else:
         stadiumDAO.listStadium()
-    return render_template("stadionok.html", stadiums=database.cursor)
+    return render_template("stadionok.html", stadiums=database.cursor, error=error)
 
 
 @app.route('/training_grounds', methods=['GET', 'POST'])
-def scorers():
+def training_grounds():
+    error = None
     if request.method == "POST":
         trainingGroundsDAO.listTrainingGrounds()
         if request.form.get('list') is not None and request.form.get('search_word').strip() != "":
             trainingGroundsDAO.searchTrainingGround(request.form.get('search_word'))
         elif request.form.get('update') is not None:
-            trainingGroundsDAO.updateTrainingGround(name=request.form.get('training_ground'),
-                                                    team=request.form.get('teams'),
-                                                    city=request.form.get('city'))
+            try:
+                trainingGroundsDAO.updateTrainingGround(name=request.form.get('training_ground'),
+                                                        team=request.form.get('teams'),
+                                                        city=request.form.get('city'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
         elif request.form.get('remove_record') is not None:
             trainingGroundsDAO.removeTrainingGround(request.form.get('record_key'))
         elif request.form.get('create') is not None:
-            trainingGroundsDAO.createTrainingGround(name=request.form.get('training_ground'),
-                                                    team=request.form.get('teams'),
-                                                    city=request.form.get('city'))
+            try:
+                trainingGroundsDAO.createTrainingGround(name=request.form.get('training_ground'),
+                                                        team=request.form.get('teams'),
+                                                        city=request.form.get('city'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
     else:
         trainingGroundsDAO.listTrainingGrounds()
-    return render_template("training_grounds.html", grounds=database.cursor)
+    return render_template("training_grounds.html", grounds=database.cursor, error=error)
 
 
 @app.route('/managers', methods=['GET', 'POST'])
 def manager():
+    error = None
     if request.method == "POST":
         managersDAO.listManagers()
         if request.form.get('list') is not None and request.form.get('search_word').strip() != "":
             managersDAO.searchManager(request.form.get('search_word'))
         elif request.form.get('update') is not None:
-            managersDAO.updateManager(  manager_id=request.form.get('managers_id'),
-                                        team=request.form.get('teams_name'),
-                                        nationality=request.form.get('nationality'),
-                                        name=request.form.get('name'))
+            try:
+                managersDAO.updateManager(manager_id=request.form.get('managers_id'),
+                                          team=request.form.get('teams_name'),
+                                          nationality=request.form.get('nationality'),
+                                          name=request.form.get('name'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
         elif request.form.get('remove_record') is not None:
             managersDAO.removeManager(request.form.get('record_key'))
         elif request.form.get('create') is not None:
-            managersDAO.createManager(team=request.form.get('teams_name'),
-                                      nationality=request.form.get('nationality'),
-                                      name=request.form.get('name'))
+            try:
+                managersDAO.createManager(team=request.form.get('teams_name'),
+                                          nationality=request.form.get('nationality'),
+                                          name=request.form.get('name'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
     else:
         managersDAO.listManagers()
-    return render_template("managers.html", managers=database.cursor)
+    return render_template("managers.html", managers=database.cursor, error=error)
 
 
 @app.route('/owners', methods=['GET', 'POST'])
 def owners():
+    error = None
     if request.method == "POST":
         ownersDAO.listOwners()
         if request.form.get('list') is not None and request.form.get('search_word').strip() != "":
             ownersDAO.searchOwner(request.form.get('search_word'))
         elif request.form.get('update') is not None:
-            ownersDAO.updateOwner(request.form.get('owner_id'),
-                                  request.form.get('company'),
-                                  request.form.get('teams_name'),
-                                  request.form.get('owner_name'))
+            try:
+                ownersDAO.updateOwner(owner_id=request.form.get('owner_id'),
+                                      company=request.form.get('company'),
+                                      team=request.form.get('teams_name'),
+                                      name=request.form.get('owner_name'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
         elif request.form.get('remove_record') is not None:
             ownersDAO.removeOwner(request.form.get('record_key'))
         elif request.form.get('add') is not None:
-            ownersDAO.createOwner(request.form.get('company'),
-                                  request.form.get('teams_name'),
-                                  request.form.get('owner_name'))
+            try:
+                ownersDAO.createOwner(company=request.form.get('company'),
+                                      team=request.form.get('teams_name'),
+                                      name=request.form.get('owner_name'))
+            except mysql.connector.errors.IntegrityError:
+                error = "Integrity Error occured(duplicate primary key or not existing foreign key"
     else:
         ownersDAO.listOwners()
-    return render_template("owners.html", owners=database.cursor)
+    return render_template("owners.html", owners=database.cursor, error=error)
 
 
 @app.route('/stat')
 def statistics():
-    return render_template("statistics.html", teams=clubsDAO.scorersByTeam(),
-                           sumPlayersTeam=playersDAO.sumPlayersByTeam())
+    return render_template("statistics.html", scorers=playersDAO.topScorers(),
+                           sumPlayersTeam=playersDAO.sumPlayersByTeam(),
+                           teams=playersDAO.groupScorersByTeam())
 
 
 if __name__ == '__main__':
